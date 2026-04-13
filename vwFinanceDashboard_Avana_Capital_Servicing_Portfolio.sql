edcbc101a17976de1cf606671fe54880b783f53d
prashj2026 <prashant.joshi@avanacompanies.com>
2026-04-13 23:18:41 +0530

    changes related to TMO

--- vwFinanceDashboard_Avana_Capital_Servicing_Portfolio.sql
+++ vwFinanceDashboard_Avana_Capital_Servicing_Portfolio.sql
@@ -1,12 +1,23 @@
 CREATE VIEW `avana-data-warehouse-prod.ACUMATICA.vwFinanceDashboard_Avana_Capital_Servicing_Portfolio` AS 
-SELECT 'Avana Capital' as AcctName,
-  CAST(FORMAT_DATE('%Y' ,LAST_DAY(AsOfDate)) AS INT64) AS FinYear,
-  CAST(FORMAT_DATE('%Y%m' ,LAST_DAY(AsOfDate)) AS INT64) AS FinPeriodID,          
-  PARSE_DATE("%Y%m", CAST(CAST(FORMAT_DATE('%Y%m' ,LAST_DAY(AsOfDate)) AS INT64) AS STRING)) AS StartDateUI,
-  IFNULL(SUM(ExpAmt),0) AS Actual_Servicing_Portfolio
-FROM `TMO.DailyExposures` -- Avana Capital
-WHERE  AsOfDate = LAST_DAY(AsOfDate) 
-GROUP BY 
-  CAST(FORMAT_DATE('%Y' ,LAST_DAY(AsOfDate)) AS INT64) ,
-  CAST(FORMAT_DATE('%Y%m' ,LAST_DAY(AsOfDate)) AS INT64) ,          
-  PARSE_DATE("%Y%m", CAST(CAST(FORMAT_DATE('%Y%m' ,LAST_DAY(AsOfDate)) AS INT64) AS STRING)) 
+-- Monthly servicing portfolio balance for Avana Capital
+WITH base AS (
+    SELECT
+        LAST_DAY(CAST(ReportDate AS DATE)) AS period_end_date,
+        src.LoanNumber,
+        src.PrincipalBalance
+    FROM `avana-data-warehouse-prod.TMO.LoanTape` src
+    WHERE CAST(ReportDate AS DATE) = LAST_DAY(CAST(ReportDate AS DATE))
+)
+SELECT
+    'Avana Capital'                                               AS AcctName,
+    CAST(FORMAT_DATE('%Y',   period_end_date) AS INT64)           AS FinYear,
+    CAST(FORMAT_DATE('%Y%m', period_end_date) AS INT64)           AS FinPeriodID,
+    PARSE_DATE('%Y%m', FORMAT_DATE('%Y%m', period_end_date))      AS StartDateUI,
+    IFNULL(SUM(PrincipalBalance), 0)                              AS Actual_Servicing_Portfolio
+FROM base
+GROUP BY
+    CAST(FORMAT_DATE('%Y',   period_end_date) AS INT64),
+    CAST(FORMAT_DATE('%Y%m', period_end_date) AS INT64),
+    PARSE_DATE('%Y%m', FORMAT_DATE('%Y%m', period_end_date))
+  
+
